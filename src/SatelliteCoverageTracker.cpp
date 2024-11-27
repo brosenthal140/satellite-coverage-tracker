eb3a33330dfb3a921df9773e7fd653bd5d7fe9d6
Brandon Rosenthal <brosenthal140@ufl.edu>
2024-11-27 10:57:09 -0500

    Adds method definitions to SatelliteCoverageTracker class

--- src/SatelliteCoverageTracker.cpp
+++ src/SatelliteCoverageTracker.cpp
@@ -1,1 +1,18 @@
 #include "SatelliteCoverageTracker.h"
+
+/* =============== PUBLIC METHODS =============== */
+/* ---------------------------------------------- */
+/* ========== CONSTRUCTORS/DESTRUCTORS ========== */
+template <typename T>
+SatelliteCoverageTracker<T>::SatelliteCoverageTracker(const string &pathToData, const DateTime &dateTime, const CoordGeodetic &location)
+{
+	// TODO: Implement constructor
+}
+
+/* =============== PRIVATE METHODS =============== */
+/* ---------------------------------------------- */
+template <typename T>
+void SatelliteCoverageTracker<T>::_importData()
+{
+	// TODO: Implement internal import method
+}
