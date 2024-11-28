31df2407708525aead44a20ffdd052be491c8981
Brandon Rosenthal <brosenthal140@ufl.edu>
2024-11-28 11:22:25 -0600

    Refactors several functions in the GraphModel class

--- src/GraphModel.h
+++ src/GraphModel.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <string>
 #include <map>
+#include <set>
+#include <vector>
+#include <unordered_map>
 #include "SGP4.h"
 
 using namespace std;
@@ -10,6 +13,7 @@ class GraphModel {
 public:
 	/* ========== STRUCTS & ENUMS ========== */
 	struct Vertex {
+		bool isWaypoint;
 		CoordGeodetic position;
 	};
 
@@ -17,26 +21,31 @@ public:
 	GraphModel(string &directory, double &threshold);
 
 	/* ========== PUBLIC MUTATORS ========== */
-	void insertEdge(Tle &tle);
+	void insertVertex(Tle &tle);
 
 	/* ========== PUBLIC ACCESSORS ========== */
-	int waypointCount() const;
+	[[nodiscard]] int waypointCount() const;
 
 	/* ========== PUBLIC TEST METHODS ========== */
-	Vertex testGetNearestWaypoint(const CoordGeodetic &pos);
+	const Vertex& testGetNearestWaypoint(const CoordGeodetic &pos);
 
 private:
 	/* ========== PRIVATE MEMBER VARIABLES ========== */
 	string _dataDirectory;
-	map<int, Vertex> _waypoints;
+	unordered_map<int, Vertex> _vertices;
+	unordered_map<int, vector<int>> _adjacencyList;
+	set<int> _waypoints;
 	map<double, int> _wpLatMap;
 	map<double, int> _wpLongMap;
 	double _wpThreshold;
 	int _wpCount;
+	int _vertexCount;
 
 	/* ========== PRIVATE INSERTION METHODS ========== */
+	int _getNextVertexIndex();
+	int _insertVertex(const CoordGeodetic &pos, bool isWaypoint);
 	const Vertex& _insertWaypoint(const CoordGeodetic &pos);
-	void _insertEdge(Tle &tle);
+	void _insertEdge(int &from, int &to);
 
 	/* ========== PRIVATE SEARCH METHODS ========== */
 	const Vertex& _getNearestWaypoint(const CoordGeodetic &pos);
