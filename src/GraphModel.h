401c2c45f89a8b0ca8d085afff197e62a065e95c
Brandon Rosenthal <brosenthal140@ufl.edu>
2024-11-29 07:48:38 -0600

    Updates tests, changes from unordered_map to map for certain member variables

--- src/GraphModel.h
+++ src/GraphModel.h
@@ -1,9 +1,9 @@
 #pragma once
 #include <string>
 #include <map>
-#include <set>
+#include <unordered_set>
 #include <vector>
-#include <unordered_map>
+#include <map>
 #include "SGP4.h"
 
 using namespace std;
@@ -11,14 +11,21 @@ using namespace libsgp4;
 
 class GraphModel {
 public:
-	/* ========== STRUCTS & ENUMS ========== */
+	/* ========== PUBLIC STRUCTS & ENUMS ========== */
 	struct Vertex {
 		bool isWaypoint;
 		CoordGeodetic position;
+
+		bool operator==(const Vertex &rhs) const;
+	};
+
+	struct Edge {
+		int index;
+		double weight;
 	};
 
 	/* ========== CONSTRUCTORS/DESTRUCTORS ========== */
-	GraphModel(string &directory, double &threshold);
+	GraphModel(string &directory, const double &sepThresh);
 
 	/* ========== PUBLIC MUTATORS ========== */
 	void insertVertex(Tle &tle);
@@ -27,27 +34,34 @@ public:
 	[[nodiscard]] int waypointCount() const;
 
 	/* ========== PUBLIC TEST METHODS ========== */
-	const Vertex& testGetNearestWaypoint(const CoordGeodetic &pos);
+	static const Vertex& testGetNearestWaypoint(string &dataDirectory, const double &wpSepThresh, const CoordGeodetic &pos, const Vertex &refWaypoint);
+	static const vector<Edge> testConnectWaypoint(string &dataDirectory, const double &wpSepThresh, const CoordGeodetic &pos);
 
 private:
 	/* ========== PRIVATE MEMBER VARIABLES ========== */
 	string _dataDirectory;
-	unordered_map<int, Vertex> _vertices;
-	unordered_map<int, vector<int>> _adjacencyList;
-	set<int> _waypoints;
+
+	map<int, Vertex> _vertices;
+	map<int, vector<Edge>> _vertexAdjList;
+	int _vertexCount;
+
+	map<int, vector<Edge>> _wpAdjList;
+	unordered_set<int> _waypoints;
 	map<double, int> _wpLatMap;
 	map<double, int> _wpLongMap;
-	double _wpThreshold;
+	double _wpSeparation;
 	int _wpCount;
-	int _vertexCount;
 
 	/* ========== PRIVATE INSERTION METHODS ========== */
 	int _getNextVertexIndex();
 	int _insertVertex(const CoordGeodetic &pos, bool isWaypoint);
 	const Vertex& _insertWaypoint(const CoordGeodetic &pos);
-	void _insertEdge(int &from, int &to);
+	void _insertEdge(const int &from, const int &to);
+	void _insertWpEdge(const int &from, const int &to);
+	void _connectWaypoint(const int &index);
 
 	/* ========== PRIVATE SEARCH METHODS ========== */
 	const Vertex& _getNearestWaypoint(const CoordGeodetic &pos);
+	unordered_set<int> _getWaypointsInRange(const int &sourceIndex, const double &range);
 };
 
