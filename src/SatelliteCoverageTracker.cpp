66f51149fcd0aaf453abd9f1462cb247933e1348
Kamarii Austin <148893234+Kamariiaustin1@users.noreply.github.com>
2024-12-02 02:36:56 -0500

    Update SatelliteCoverageTracker.cpp

--- src/SatelliteCoverageTracker.cpp
+++ src/SatelliteCoverageTracker.cpp
@@ -1,18 +1,41 @@
 #include "SatelliteCoverageTracker.h"
+#include "TLEParser.h"  
+#include <iostream>
+#include <string>
+using namespace std; 
 
 /* =============== PUBLIC METHODS =============== */
 /* ---------------------------------------------- */
+void SatelliteCoverageTracker::importDataFromLocalFile(const string& inputFilePath) {
+    TLEParser parser;
+    const string outputPath = "data/tle_latest.txt";
+
+    //--- Retrieves TLE datat from file ---------//
+    bool success = parser.fetchTLEDataFromFile(inputFilePath, outputPath);
+    if (success) {
+        string complete_statement; 
+        complete_statement = "Successfully retrieved TLE data.";
+        cout << complete_statement << endl;
+        parser.parseTLE(outputPath);
+    }
+    else {
+        string incomplete_statement;
+        incomplete_statement = "Unsuccessfully retrieved TLE data";
+        cout << incomplete_statement << endl;
+    }
+}
+
 /* ========== CONSTRUCTORS/DESTRUCTORS ========== */
-template <typename T>
-SatelliteCoverageTracker<T>::SatelliteCoverageTracker(const string &pathToData, const DateTime &dateTime, const CoordGeodetic &location)
+
+SatelliteCoverageTracker::SatelliteCoverageTracker(const string &pathToData, const DateTime &dateTime, const CoordGeodetic &location)
 {
 	// TODO: Implement constructor
 }
 
 /* =============== PRIVATE METHODS =============== */
 /* ---------------------------------------------- */
-template <typename T>
-void SatelliteCoverageTracker<T>::_importData()
+
+void SatelliteCoverageTracker::_importData()
 {
 	// TODO: Implement internal import method
 }
